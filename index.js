b70ba4cd8ccc016e59d033599ef4024f07d4ab6e
Aldrin <113603063+aldringwapo00@users.noreply.github.com>
2026-01-13 21:14:11 +0800

    Accordion/FAQ with JAVASCRIPT!!!!!! FINALLYYYYYYYYY!

---
+++ index.js
@@ -0,0 +1,33 @@
+
+// const toggle = document.querySelector(".accordion__toggle");
+// const faq_answer = document.querySelector(".accordion__answer");
+
+
+// document.addEventListener("click", function(){
+//     const accordion_question = document.querySelectorAll(".accordion__question");
+    
+// });
+
+
+// const accordion = document.querySelector(".accordion__question");
+// const answer = document.querySelector(".accordion__answer");
+
+// function handleClick(){
+//     answer.classList.toggle('active');
+// }
+
+// accordion.addEventListener('click', handleClick);
+
+const accordion = document.querySelectorAll('.accordion__question');
+
+accordion.forEach(question =>{
+    question.addEventListener("click", () =>{
+        const answer = question.nextElementSibling;
+        const icon = question.querySelector('.accordion__toggle');
+
+        answer.classList.toggle('active');
+        question.classList.toggle('active');
+        icon.classList.toggle('active');
+    });
+});
+
